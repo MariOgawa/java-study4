104e3084cf984724d8140acc3149327d470902fd
MariOgawa <g35xc@yahoo.co.jp>
2024-08-25 00:01:41 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/student/management7/StudentManagement7/service/StudentService.java

--- src/main/java/student/management7/StudentManagement7/repository/StudentRepository.java
+++ src/main/java/student/management7/StudentManagement7/repository/StudentRepository.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import student.management7.StudentManagement7.data.Student;
 import student.management7.StudentManagement7.data.StudentsCourses;
 
@@ -24,8 +25,14 @@ public interface StudentRepository {
     @Select("SELECT * FROM students")
     List<Student> search();
 
+    @Select("SELECT * FROM students WHERE id = #{id}")
+    Student searchStudent(String id);
+
     @Select("SELECT * FROM students_courses")
-    List<StudentsCourses> searchStudentsCourse();
+    List<StudentsCourses> searchStudentsCoursesList();
+
+    @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
+    List<StudentsCourses> searchStudentsCourses(String studentId);
 
     @Insert("INSERT INTO students(name, kana_name, nickname, email, area, age, sex, remark, is_deleted) "
         + "VALUES(#{name}, #{kanaName}, #{nickname}, #{email}, #{area}, #{age}, #{sex}, #{remark}, false)")
@@ -36,4 +43,11 @@ public interface StudentRepository {
         + "VALUES(#{studentId}, #{courseName}, #{courseStartAt}, #{courseEndAt})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void registerStudentsCourses(StudentsCourses studentsCourses);
+
+    @Update("UPDATE students SET name = #{name}, kana_name = #{kanaName}, nickname = #{nickname}, "
+        + "email = #{email}, area = #{area}, age = #{age}, sex = #{sex}, remark = #{remark}, is_deleted = #{isDeleted} WHERE id = #{id}")
+    void updateStudent(Student student);
+
+    @Update("UPDATE students_courses SET course_name = #{courseName} WHERE id =#{id}")
+    void updateStudentsCourses(StudentsCourses studentsCourses);
 }
