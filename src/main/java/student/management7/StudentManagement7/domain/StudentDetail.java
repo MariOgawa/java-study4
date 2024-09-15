f5293c5014ec2ccc4e53b4178138517f42625cc7
MariOgawa <g35xc@yahoo.co.jp>
2024-09-15 14:49:34 +0900

    Merge remote-tracking branch 'origin/kadai17-3' into kadai17-3

--- src/main/java/student/management7/StudentManagement7/domain/StudentDetail.java
+++ src/main/java/student/management7/StudentManagement7/domain/StudentDetail.java
@@ -1,13 +1,17 @@
 package student.management7.StudentManagement7.domain;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import student.management7.StudentManagement7.data.Student;
 import student.management7.StudentManagement7.data.StudentsCourses;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentDetail {
 
   private Student student;
